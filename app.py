bf79cec2f7c4f586da49965f20ef0314577b23c2
Bruna Roberta <144182969+brunartt@users.noreply.github.com>
2026-03-02 17:50:14 -0300

    deploy: MVP chatbot vercel

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from routes.webhook import webhook_bp
 from config.settings import DEBUG
 
 app = Flask(__name__)
+app.register_blueprint(webhook_bp)
 
 @app.route("/")
 def home():
     return "Servidor do bot funcionando ✅"
 
-if __name__ == "__main__":
-    app.run(debug=True)
-app.register_blueprint(webhook_bp)
 
 if __name__ == "__main__":
     app.run(debug=DEBUG)
+
+    handler = app
